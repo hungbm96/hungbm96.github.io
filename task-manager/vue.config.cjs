daa6422a756080a80661c5073f217085086fe8cd
Bùi Mạnh Hưng <hungbm2@rikkeisoft.com>
2024-06-12 15:26:39 +0700

    vercel 2.0

--- task-manager/vue.config.cjs
+++ task-manager/vue.config.cjs
@@ -1,6 +1,3 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/hungbm96.github.io/" // Thay 'tên-repo-của-bạn' bằng tên repository trên GitHub của bạn
-      : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
 };
